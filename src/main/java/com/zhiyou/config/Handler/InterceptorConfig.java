0bc91f04d1f98994f7f12e277d569fcfd66beb31
sununshine <1274048194@qq.com>
2019-08-17 16:42:46 +0800

    sununshine

---
+++ src/main/java/com/zhiyou/config/Handler/InterceptorConfig.java
@@ -0,0 +1,19 @@
+package com.zhiyou.config.Handler;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer{
+	
+	//添加拦截器
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		//拦截所有，除了user/index
+		registry.addInterceptor(new MyInterceptor()).addPathPatterns("/course/**","/speaker/**","/admin/**","/video/**")
+		.excludePathPatterns("/adminLogin.do");
+	}
+
+	
+}
